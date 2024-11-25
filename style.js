8143c72460f3d3532855dc6c09ec29c43ce52ee7
kesavan200314 <vijaykesa1114@gmail.com>
2024-11-25 09:57:16 +0530

    Update style.js

--- style.js
+++ style.js
@@ -14,7 +14,8 @@ form.addEventListener('submit', e => {
 }
 const setSuccess = element => {
     const inputControl = element.parentElement;
-    const errorDisplay = inputControl.querySelector('.error');    errorDisplay.innerText = '';
+    const errorDisplay = inputControl.querySelector('.error');   
+    errorDisplay.innerText = '';
     inputControl.classList.add('success');
     inputControl.classList.remove('error');
 };const isValidEmail = email => {
